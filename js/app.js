c0923960398096a5f1fe8050757abb846b988e28
Marques <marquesviniciusmelomartins@gmail.com>
2025-04-26 20:54:05 -0300

    Atualizar endpoint para backend online

--- js/app.js
+++ js/app.js
@@ -414,7 +414,7 @@ Instruções adicionais:
 // Função para chamar o servidor local Python
 async function fetchBackstoryFromLocal(prompt) {
     try {
-        const response = await fetch('http://127.0.0.1:5000/generate', {
+        const response = await fetch('https://forjador-backend.onrender.com/generate', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
